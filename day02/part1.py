6abef2fb0af9b1015bad846b9f3c9060f5bb71fd
Collin Brittle <cnbrittle@gmail.com>
2023-12-05 23:02:58 -0500

    Solves day 2 , part 1
    Not knowing what is in part two, it seems prudent to load all the data
    in a way that I can ask questions of it later. I don't work with SQL
    often, but the shape of the data is easy enough to model, and now I
    can query it with less hassle.

---
+++ day02/part1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import sqlite3, re
+import pdb
+
+GAME_ID = re.compile(r'\d+$')
+RED = re.compile(r'(\d+) red')
+BLUE = re.compile(r'(\d+) blue')
+GREEN = re.compile(r'(\d+) green')
+
+def extract_data(line):
+    game = []
+
+    game_id, _, subsets = line.partition(': ')
+    game_id = int(GAME_ID.search(game_id).group())
+
+    subsets = subsets.strip().split('; ')
+    for subset in subsets:
+        red = RED.search(subset)
+        blue = BLUE.search(subset)
+        green = GREEN.search(subset)
+
+        if red is not None:
+            red = int(red.group(1))
+        else:
+            red = 0
+        if blue is not None:
+            blue = int(blue.group(1))
+        else:
+            blue = 0
+        if green is not None:
+            green = int(green.group(1))
+        else:
+            green = 0
+
+        game.append((game_id, red, blue, green))
+
+    return game
+
+if __name__ == '__main__':
+    connection = sqlite3.connect(':memory:')
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE game(id, red, blue, green)')
+
+    data = []
+    with open('./input.txt') as inputfile:
+        for line in inputfile:
+            data.extend(extract_data(line))
+
+    cursor.executemany('INSERT INTO game VALUES (?, ?, ?, ?)', data)
+    connection.commit()
+
+    all_ids = set()
+    for row in cursor.execute('SELECT DISTINCT id FROM game'):
+        all_ids.add(row['id'])
+
+    illegals = set()
+    for row in cursor.execute('SELECT id FROM game WHERE red > 13 OR blue > 14 OR green > 13'):
+        illegals.add(row['id'])
+
+    print(sum(all_ids - illegals))
+
+    connection.close()
